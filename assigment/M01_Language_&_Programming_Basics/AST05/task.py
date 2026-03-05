59063ba3b74832ec660cb88afd884c7df9518dd4
Sai <saigch176@gmail.com>
2026-03-05 16:29:27 +0530

    Assignment-5

---
+++ assigment/M01_Language_&_Programming_Basics/AST05/task.py
@@ -0,0 +1,14 @@
+def Collatz_Sequence(n: int) -> list:
+    if n <= 0:
+        raise ValueError("Input must be a positive integer")
+
+    sequence = [n]
+
+    while n != 1:
+        if n % 2 == 0:
+            n = n // 2
+        else:
+            n = 3 * n + 1
+        sequence.append(n)
+
+    return sequence
